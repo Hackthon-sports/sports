e7425f74426710d49ea68cf5d0a9f234bc497409
pavan-kotni2005 <kotnipavan8@gmail.com>
2025-07-28 00:55:18 +0530

    setup

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-purple-100"
+      
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
